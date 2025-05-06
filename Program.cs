5d9a2c522dc00d91d0afd68ec8758896e621df90
mentalhooligan <untro999@yandex.ru>
2025-05-06 21:17:09 +0400

    setup  for thread based branch

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ namespace IntervalTimer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            Application.Run(new Form1() { IsMdiContainer = true });
         }
     }
 }
